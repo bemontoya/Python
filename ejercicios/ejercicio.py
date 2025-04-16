4e4ad8935a011a4b94bcf1669fafa535d0a4c630
bemontoya <be.montoya@duocuc.cl>
2025-04-16 17:55:46 -0400

    Add files via upload

---
+++ ejercicios/ejercicio.py
@@ -0,0 +1,20 @@
+print("Bienvenido al mundo de la programación. ")
+nom = input("Para comenzar, ingresa tu nombre:" )
+
+print(f"Bienvenido {nom}")
+
+x = int(input("Ingrese un número: "))
+print("")
+resultado = (x**2 + 3 * x + 1) / 4
+print(f"El resultado de la ecuación es {resultado}")
+
+
+nomApellido = input("Ingrese su nombre y apellido: ")
+rut = int(input("Ingrese su rut: "))
+correo = input("Ingrese su correo: ")
+telefono = int(input("Ingrese su teléfono: "))
+
+print(f"NOMBRE:           {nomApellido}")
+print(f"RUT:              {rut}")
+print(f"CORREO:           {correo}")
+print(f"TELEFONO:         {telefono}")
